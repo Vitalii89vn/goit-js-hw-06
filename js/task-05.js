e6f22fc88edcbcd70715658ed1d1ecfa4d0a375d
Vitalii89vn <vitalii.kuhta@gmail.com>
2022-10-18 18:07:21 +0300

    5+6

--- js/task-05.js
+++ js/task-05.js
@@ -3,10 +3,16 @@ const outputField = document.querySelector('#name-output');
 
 console.log(inputField.textContent)
 inputField.addEventListener('input', event => {
-    if (inputField.textContent === null) {
-        return outputField.textContent;
-    }         outputField.textContent = event.currentTarget.value;
-
-        
-
+    // outputField.textContent = event.currentTarget.value;
+  
+  if (!event.currentTarget.value) {
+        outputField.textContent;
+    }    outputField.textContent = event.currentTarget.value;
 })
+
+
+
+
+  // if (textInput.value = "") {
+  //       outputField.textContent;
+  //   }   outputField.textContent = event.currentTarget.value;
---
+++ js/task-06.js
@@ -0,0 +1,9 @@
+const inputValue = document.querySelector('#validation-input');
+console.log(inputValue);
+
+const sssss = inputValue.addEventListener('blur', event => {
+    if (inputValue.currentTarget.value.length = inputValue[data - length]) {
+        #validation-input.valid
+    }#validation-input.invalid
+})
+// if(inputValue.currentTarget.value.length = inputValue[data-length])
