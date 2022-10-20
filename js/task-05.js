f84ae05a9aa852cc6c8badf1a11d8f1153d342d3
Vitalii89vn <vitalii.kuhta@gmail.com>
2022-10-20 17:09:14 +0300

    task 6

--- js/task-05.js
+++ js/task-05.js
@@ -3,16 +3,10 @@ const outputField = document.querySelector('#name-output');
 
 console.log(inputField.textContent)
 inputField.addEventListener('input', event => {
-    // outputField.textContent = event.currentTarget.value;
-  
-  if (!event.currentTarget.value) {
-        outputField.textContent;
-    }    outputField.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === ``) {
+         outputField.textContent;
+      }   outputField.textContent = event.currentTarget.value;
+ 
 })
 
 
-
-
-  // if (textInput.value = "") {
-  //       outputField.textContent;
-  //   }   outputField.textContent = event.currentTarget.value;
--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,19 @@
 const inputValue = document.querySelector('#validation-input');
+// const dataValue = document.querySelector('[data-length="6"]')
 console.log(inputValue);
 
 const sssss = inputValue.addEventListener('blur', event => {
-    if (inputValue.currentTarget.value.length = inputValue[data - length]) {
-        #validation-input.valid
-    }#validation-input.invalid
+    if (event.currentTarget.value.length === inputValue.dataset.length) {
+    event.classList.replace('#validation-input', '#validation-input.valid');
+  } event.classList.replace('#validation-input', '#validation-input.invalid');
+    
 })
+
+
+
+
+    // if (inputValue.currentTarget.value.length === inputValue.data - length.valueOf) {
+    //     input.classList.add('validation-input.valid');
+    // }input.classList.add('validation-input.invalid');
+
 // if(inputValue.currentTarget.value.length = inputValue[data-length])
