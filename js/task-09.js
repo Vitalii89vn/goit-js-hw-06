c645db58b9f76d2f4db589b77e202e91b3f232aa
Vitalii89vn <vitalii.kuhta@gmail.com>
2022-10-21 13:28:22 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector('body');
+const spanText = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color')
+
+changeColorBtn.addEventListener('click', changeBackgroundColor);
+
+function changeBackgroundColor(event) {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  spanText.textContent = `${getRandomHexColor()}`;
+  
+}
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const inputNumber = document.querySelector('[type="number"]');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesField = document.querySelector('#boxes');
+
+
+
+console.log(Number(inputNumber.currentTarget.value));
+
+// createBtn.addEventListener('submit', event => {
+//   event.preventDefault();
+//   createBoxes();
+// })
+
+
+
+// function createBoxes(amount) {
+//   amount = Number(inputNumber.currentTarget.value);
+  
+//   console.log(amount);
+//   const boxes = 
+//   boxesField.insertAdjacentElement('afterbegin', boxes)
+//   let count = 30;
+//   boxesField.style.backgroundColor = `${getRandomHexColor}`;
+//   boxesField.style.border = "2px solid #000000";
+//   boxesField.style.width = `(${count}+=10)px`;
+//   boxesField.style.heigth = `(${count}+=10)px`;
+
+// }
