fa19b2caaf0b14a9056d6b24cab859923813bd4d
Vitalii89vn <vitalii.kuhta@gmail.com>
2022-10-27 17:03:11 +0300

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -5,42 +5,38 @@ const inputNumber = document.querySelector('[type="number"]');
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const boxesField = document.querySelector('#boxes');
-const createdBoxes = boxesField.children;
+const createdBoxes = document.querySelectorAll('.box');
 
 createBtn.addEventListener('click', createBoxes());
 destroyBtn.addEventListener('click', destroyBoxes());
 
+
 function createBoxes(amount) {
-   inputNumber.addEventListener('input', event => {
-     amount = Number(event.currentTarget.value);
-   
-     let counter = [];
-     for (let i = 1; i <= amount; i += 1) {
-       counter.push(i);
-     }
-       const code = counter.map(element => {
-        let startCount = 20;
-        let currentSize = startCount += 10*element;
-        return `<div class="box" style = " background-color: ${getRandomHexColor()} ; border: 2px solid rgb(0, 0, 0); width: ${currentSize}px; height: ${currentSize}px; " ></div >`
-     }).join('');
+  inputNumber.addEventListener('input', event => {
+    amount = Number(event.currentTarget.value);
+
+    let counter = [];
+    for (let i = 1; i <= amount; i += 1) {
+      counter.push(i);
+    }
+    const code = counter.map(element => {
+      let startCount = 20;
+      let currentSize = startCount += 10 * element;
+      return `<div class="box" style = " background-color: ${getRandomHexColor()} ; border: 2px solid rgb(0, 0, 0); width: ${currentSize}px; height: ${currentSize}px; " ></div >`
+    }).join('');
       
-       boxesField.insertAdjacentHTML('beforeend', code) 
+    boxesField.insertAdjacentHTML('beforeend', code)
   })
 }
-
+  
 function destroyBoxes() {
- createdBoxes.innerHTML= '';
+ createdBoxes.innerHTML = '';
   }
   
-    // boxesField.style.backgroundColor = `${getRandomHexColor()}`;
-    // boxesField.style.border = "2px solid #000000";
-    // boxesField.style.width = currentSize + `px`;
-    // boxesField.style.heigth = currentSize + `px`;
-    
-    //  const box = `<div class="box" style = " background-color: ${getRandomHexColor()} ; border: 2px solid rgb(0, 0, 0); width: ${currentSize}px; height: ${currentSize}px; " ></div >`;
-    //  const boxes = box.repeat(amount);
-    //  boxesField.innerHTML('beforeend', box)
 
-    
 
-// console.log(createBoxes())
+// function getCurrentRange(range) {
+//   inputNumber.addEventListener('input', event => {
+//     range = Number(event.currentTarget.value);
+//   })
+// }
